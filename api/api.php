6de6c6df48fef09d0513cbd19f123c98962382fb
fannyhasbi <fnyhsbi@gmail.com>
2018-11-30 02:56:31 +0700

    Make bus data dynamic

--- api/api.php
+++ api/api.php
@@ -1,12 +1,35 @@
 <?php
 
 class Api {
+  private $koneksi;
+
+  public function __construct(){
+    $host = getenv('BSR_DB_HOST');
+    $user = getenv('BSR_DB_USER');
+    $pass = getenv('BSR_DB_PASS');
+    $db   = getenv('BSR_DB_NAME');
+
+    $this->koneksi = mysqli_connect($host, $user, $pass, $db) or die(mysql_error());
+  }
+
+  public function __destroy(){
+    mysqli_close($this->koneksi);
+  }
+
   public function getBus(){
     $data = array(
       'status' => 200,
-      'message'=> 'OK',
-      'data' => null
+      'message'=> 'OK'
     );
+
+    $result = mysqli_query($this->koneksi, "SELECT * FROM perusahaan");
+
+    while($r = mysqli_fetch_assoc($result)){
+      $data['data'][] = array(
+        'id' => $r['id'],
+        'nama' => $r['nama']
+      );
+    }
 
     Flight::json($data);
   }
