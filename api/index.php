5ac8aa807b0644c363f56858c45ffad42cfd3577
fannyhasbi <fnyhsbi@gmail.com>
2018-11-30 02:06:15 +0700

    Split route into new file

--- api/index.php
+++ api/index.php
@@ -5,8 +5,7 @@ require_once __DIR__."/../vendor/autoload.php";
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-Flight::route('/', function(){
-  echo "Welcome to Bus Schedule REST API!";
-});
+// Load routes
+require_once __DIR__."/routes.php";
 
 Flight::start();
---
+++ api/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+Flight::route('/', function(){
+  echo "Welcome to Bus Schedule REST API!";
+});
