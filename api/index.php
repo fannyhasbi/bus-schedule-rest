e27553b650ba5a8c77c483106c893385c78e9423
fannyhasbi <fnyhsbi@gmail.com>
2018-11-30 02:03:51 +0700

    Setup Flight microframework

--- api/index.php
+++ api/index.php
@@ -1,5 +1,12 @@
 <?php
 require_once __DIR__."/../vendor/autoload.php";
 
+// Load .env
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
+
+Flight::route('/', function(){
+  echo "Welcome to Bus Schedule REST API!";
+});
+
+Flight::start();
